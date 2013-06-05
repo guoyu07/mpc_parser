46736e786f17be2d81629644dcce610edfc16b8f
Henry Lee <manageyp@gmail.com>
2013-06-05 21:02:14 +0800

    [NewFeature] first init mpc parser project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MpcParser::Application.config.secret_token = '2d65ea0c22a9c2ad4e1bcd1d10f951bda4746d2eee6d1965eaa604538362631f7f625f1607559147f6c44cc7cbabdea25919e5bbb7226352a80055b94991ead2'
